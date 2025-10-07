389ea455117cee9682cd345fe7755c46568afe20
nichampion <nichampion72@gmail.com>
2025-10-07 14:40:33 +0200

    Formulaire : input WIP

--- projects/lib/src/reset-input/reset-input.ts
+++ projects/lib/src/reset-input/reset-input.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-input',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './reset-input.html',
-  styleUrl: './reset-input.css'
+  styleUrl: './reset-input.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: ResetInput,
+      multi: true
+    }
+  ]
 })
-export class ResetInput {
+export class ResetInput implements ControlValueAccessor {
+  @Input() placeholder = '';
+  
+value: string = '';
 
+  onInputChange(event: any): void {
+    this.value = event.target.value;
+
+    // MAJ cycle de vie du formulaire
+    this.onChange(this.value);
+    this.onTouched();
+  }
+
+  reset(): void {
+    this.value = '';
+    this.onChange(this.value);
+    this.onTouched();
+  }
+
+  // ControlValueAccessor
+
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    console.log('registerOnChange');
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    console.log('registerOnTouched');
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    console.log('setDisabledState', isDisabled);
+  }
 }
--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ResetInput } from "../../../../projects/lib/src/reset-input/reset-input";
 
 @Component({
@@ -6,8 +7,8 @@ import { ResetInput } from "../../../../projects/lib/src/reset-input/reset-input
     templateUrl: './dashboard.html',
     styleUrl: './dashboard.css',
     standalone: true,
-    imports: [ResetInput]
+    imports: [ResetInput, ReactiveFormsModule]
 })
 export class Dashboard {
-
+    formControl = new FormControl('');
 }
