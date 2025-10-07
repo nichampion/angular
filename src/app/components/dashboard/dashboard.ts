a55a9d5313b397ec21592c5d7570626704eb78d1
nichampion <nichampion72@gmail.com>
2025-10-07 15:25:30 +0200

    Formulaire : adresse WIP

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ResetInput } from "../../../../projects/lib/src/reset-input/reset-input";
+import { AuthorAddress } from '../../models/Author';
 
 @Component({
     selector: 'app-dashboard',
@@ -11,10 +12,23 @@ import { ResetInput } from "../../../../projects/lib/src/reset-input/reset-input
 })
 export class Dashboard {
     formControl = new FormControl('');
+    
+    // Définition du type correct pour le formulaire
+    authorAddressForm = new FormGroup({
+        number: new FormControl<string>(''),
+        street: new FormControl<string>(''),
+        postalCode: new FormControl<string>(''),
+        city: new FormControl<string>(''),
+        country: new FormControl<string>('')
+    });
 
     constructor() {
         this.formControl.valueChanges.subscribe(value => {
             console.log('Nouvelle marque de véhicule:', value);
+        });
+
+        this.authorAddressForm.valueChanges.subscribe((value) => {
+            console.log('Nouvelle adresse:', value);
         });
     }
 }
