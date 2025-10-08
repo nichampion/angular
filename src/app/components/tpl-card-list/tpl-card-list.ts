93fdfe97931c91e522da147103894c0fe9b7eda3
nichampion <nichampion72@gmail.com>
2025-10-08 13:51:35 +0200

    Templating WIP

---
+++ src/app/components/tpl-card-list/tpl-card-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tpl-card-list',
+  imports: [],
+  templateUrl: './tpl-card-list.html',
+  styleUrl: './tpl-card-list.css'
+})
+export class TplCardList {
+
+}
